4e92f5070abb139b791046db72ce04f330f8811c
SebastianMayG <al064488@uacam.mx>
2023-09-24 18:32:30 -0600

    Añadir los carros en el mundo

--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,15 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(900, 600, 1); 
+        super(900, 600, 1);
+        
+        addObject(new Auto(), 536, 576);
+        addObject(new Auto(), 481, 576);
+        addObject(new Auto(), 424, 576);
+        addObject(new Auto(), 361, 576);
+        addObject(new Auto(), 298, 576);
+        addObject(new Auto(), 235, 576);
+        addObject(new Auto(), 176, 576);
+        addObject(new Auto(), 115, 576);
     }
 }
