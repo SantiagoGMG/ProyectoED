45a2668bca3f0530e75db6a9b795027aa701462f
SebastianMayG <al064488@uacam.mx>
2023-09-24 18:02:26 -0600

    Crear clases hijas de Car

---
+++ Amarillo.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Amarillo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Amarillo extends Car
+{
+    /**
+     * Act - do whatever the Amarillo wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
